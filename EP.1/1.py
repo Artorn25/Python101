920261c469780e4edbac332fe008f828fbeda7a6
JesusPie <62898381+Artorn25@users.noreply.github.com>
2023-02-18 17:37:34 +0700

    add EP.1 & EP.2

---
+++ EP.1/1.py
@@ -0,0 +1,19 @@
+print('Hello Jen')
+print('Hello Dam')
+print('Hello World')
+
+print('Hello World')
+print('Hello World')
+print('Hello World')
+print('Hello World')
+print('Hello World')
+print('Hello World')
+print('Hello World')
+print('Hello World')
+print('Hello World')
+
+
+
+
+
+
---
+++ EP.2/info.py
@@ -0,0 +1,2 @@
+name = 'Jen'
+lastname = 'Damnoenudomkan'
