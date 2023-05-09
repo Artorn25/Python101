d6b105a184829464cf3634d283bdf84dd169ae28
JellyJess <62898381+Artorn25@users.noreply.github.com>
2023-05-09 17:15:21 +0700

    new version

--- EP.4/GUI Knowledge v.2.py
+++ EP.4/GUI Knowledge v.2.py
@@ -1,7 +1,6 @@
-from tkinter import *
-from tkinter import ttk  # theme of tk
+from tkinter  import *
+from tkinter import ttk  # theme of ttk
 from tkinter import messagebox
-
 import csv
 
 def writecsv(datalist):
@@ -16,15 +15,15 @@ def readcsv():
     return data
 
 GUI = Tk()  # หน้าจอของโปรเเกรม
-GUI.title('โปรเเกรมเก็บข้อมูล')  # ชื่อโปรเเกรม
-GUI.geometry('900x500')  # ขนาดโปรเเกรม
+GUI.title('Program keep data')  # ชื่อโปรเเกรม
+GUI.geometry('800x500')  # ขนาดโปรเเกรม
 
 background = Label(GUI, bg="blue")
 background.pack(fill=BOTH, expand=True)
 
-L1 = Label(GUI, text='โปรเเกรมบันทึกความรู้',
-           font=('TH Sarabun New', 20), fg='green')
-L1.place(x=250, y=50)
+L1 = Label(GUI, text='ระบบเก็บข้อมูล',
+           font=('TH Sarabun New', 20,"bold"), fg='green')
+L1.place(x=280, y=50)
 
 # image
 FB1 = LabelFrame(GUI)
@@ -34,32 +33,34 @@ label_1 = Label(FB1, image=image)
 label_1.pack()
 
 # text
-FB2 = LabelFrame(GUI,font=('TH Sarabun New', 16,"bold"))
-FB2.place(x=500, y=95)
+FB2 = LabelFrame(GUI,font=('TH Sarabun New', 14,"bold"))
+FB2.place(x=520, y=100)
 
+text = Text(FB2, font=('TH Sarabun New', 12), height=10, width=50)
 
-text = Text(FB2, font=('TH Sarabun New', 16), height=10, width=80)
-text.pack(pady=10, padx=15)
-#entry = Entry(FB2)
-#entry.pack(ipadx=50, ipady=5)
+scroll = ttk.Scrollbar(FB2, orient=VERTICAL)
+scroll.place(x=296, y=12, height=221)
 
-scroll = ttk.Scrollbar(FB2, orient=VERTICAL, command=text.yview)
-scroll.place(x=640, y=12, height=280)
 text.config(yscrollcommand=scroll.set)
+scroll.config(command=text.yview)
 
-data = readcsv()
-for row in data:
-    text.insert(END,row)
-    text.insert(END,'\n')
+text.pack(pady=10, padx=10)
 
 #
 LF1 = ttk.LabelFrame(GUI, text='กรอกข้อมูลที่ต้องการเข้าไป')
-LF1.place(x=200, y=320)
+LF1.place(x=208, y=320)
 
 v_data = StringVar()  # ตัวแปรพิเศษที่ใช้กับข้อความ
-E1 = ttk.Entry(LF1, textvariable=v_data, font=('TH Sarabun New', 18))
-E1.pack(padx=50, pady=8)
+E1 = ttk.Entry(LF1, textvariable=v_data, font=('TH Sarabun New', 16))
+E1.pack(padx=50, pady=6)
 
+#Show data
+def ShowData():
+    data = readcsv()
+    for row in data:
+        text.insert(END,row)
+        text.insert(END,'\n')
+               
 from datetime import datetime
 
 #save data
@@ -71,9 +72,11 @@ def SaveData():
     nofi = 'บันทึกเรียบร้อย'
     messagebox.showinfo('เเจ้งเตือน', nofi)
     v_data.set('') #เคลียร์ข้อมูลที่อยู่ในช่องกรอก
-
+        
 # button
-B4 = ttk.Button(LF1,text='บันทึกข้อมูล',command=SaveData)
-B4.pack(ipadx=50,ipady=5)   
+B1 = ttk.Button(LF1,text='บันทึกข้อมูล',command=SaveData)
+B1.pack(ipadx=30,ipady=5)   
 
+B2 = ttk.Button(LF1,text='เเสดงข้อมูล', command=ShowData)
+B2.pack(ipadx=30,ipady=5,padx=5,pady=5)
 GUI.mainloop()
